ed3444cb6af80341a7d960745179d741b9b1c2e5
Louis <lippoint.surf0622@gmail.com>
2018-11-17 17:51:33 +0900

    smoothing

--- js/animate.js
+++ js/animate.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-    //console.log("Hello");
-   //$(".st0").addClass("active");
-   setTimeout(function(){
-        $(".introTit").animate({opacity:'1'},2000);
-   },3000);
-   setTimeout(function(){
-        $(".visitCard>a").animate({opacity:'1'},2000)
-   },4000);
+     jQuery.Color.hook( "fill" );
+     setTimeout(function(){
+          $("#st0>path").animate({ 
+               "fill": "#000000"
+          },800);
+     },4000);
+     setTimeout(function(){
+          $(".introTit").animate({opacity:'1'},2000);
+     },4500);
+     setTimeout(function(){
+          $(".visitCard>a").animate({opacity:'1'},2000);
+     },5000);
 });
+
+
